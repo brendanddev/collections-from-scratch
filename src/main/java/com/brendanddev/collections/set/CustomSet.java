579ac0c7f8ab47c37cfe4e173668ec11a30abebb
Brendan Dileo <dileob23@gmail.com>
2025-08-23 11:59:52 -0400

    Create CustomSet

---
+++ src/main/java/com/brendanddev/collections/set/CustomSet.java
@@ -0,0 +1,11 @@
+package com.brendanddev.collections.set;
+
+import com.brendanddev.collections.core.CustomCollection;
+
+
+/**
+ * 
+ */
+public class CustomSet<T> implements CustomCollection<T> {
+    
+}
