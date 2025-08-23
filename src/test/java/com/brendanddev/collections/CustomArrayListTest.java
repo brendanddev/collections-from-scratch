a91448e3186345e72650cc8c2ef36cf4a964d8b5
Brendan Dileo <dileob23@gmail.com>
2025-08-23 11:53:41 -0400

    Add tests for adding and removing

--- src/test/java/com/brendanddev/collections/CustomArrayListTest.java
+++ src/test/java/com/brendanddev/collections/CustomArrayListTest.java
@@ -1,10 +1,41 @@
 package com.brendanddev.collections;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
 
+import com.brendanddev.collections.list.CustomArrayList;
 
 /**
- * 
+ * Unit tests for the CustomArrayList class.
+ * Tests basic functionality of the CustomArrayList implementation, including adding elements,
+ * and checking for their presence in the list.
  */
 public class CustomArrayListTest {
+
+    /**
+     * Tests the CustomArrayLists add and contains methods.
+     */
+    @Test
+    void testAddAndContains() {
+        CustomArrayList<String> customArrayList = new CustomArrayList<>();
+        assertTrue(customArrayList.add("Brendan"));
+        assertTrue(customArrayList.contains("Brendan"));
+        assertFalse(customArrayList.contains("Development"));
+    }
+
+    /**
+     * Tests the CustomArrayList's remove method.
+     */
+    @Test
+    void testRemove() {
+        CustomArrayList<String> customArrayList = new CustomArrayList<>();
+        customArrayList.add("Hello");
+        customArrayList.add("World");
+        assertTrue(customArrayList.remove("Hello"));
+        assertFalse(customArrayList.contains("Hello"));
+        assertEquals(1, customArrayList.size());
+    }
     
 }
