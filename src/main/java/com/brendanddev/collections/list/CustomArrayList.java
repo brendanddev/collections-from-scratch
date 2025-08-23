7394e1212d86af2bcdb4bfecd2d4260e8a1f412c
Brendan Dileo <dileob23@gmail.com>
2025-08-23 11:43:03 -0400

    Implement add, remove, and contains methods

--- src/main/java/com/brendanddev/collections/list/CustomArrayList.java
+++ src/main/java/com/brendanddev/collections/list/CustomArrayList.java
@@ -21,6 +21,69 @@ public class CustomArrayList<T> implements CustomCollection<T> {
         elements = new Object[DEFAULT_CAPACITY];
     }
 
+
+
+    /**
+     * Adds an element to the end of the list.
+     * If the internal array is full, the arrays capacity is doubled before adding.
+     * 
+     * @param element The element to add to the list.
+     * @return true, since the list is always modified by adding.
+     */
+    @Override
+    public boolean add(T element) {
+        if (element == null) throw new IllegalArgumentException("Null elements not allowed");
+        ensureCapacity();
+        elements[size] = element;
+        size++;
+        return true;
+    }
+
+    /**
+     * Removes the first occurrence of the specified element from the list if it exists.
+     * After removal, all subsequent elements to the left are shifted to fill the gap.
+     * 
+     * @param element The element to remove from the list.
+     * @return true if the element was found and removed, otherwise false.
+     */
+    @Override
+    public boolean remove(T element) {
+        if (isEmpty()) return false;
+
+        // Loop to find element
+        for (int i = 0; i < size; i++) {
+            if (elements[i].equals(element)) {
+                // Shift all elements to the left after the removed element
+                for (int j = i; j < size - 1; j++) {
+                    elements[j] = elements[j + 1];
+                }
+
+                elements[size - 1] = null;
+                size--;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Checks if the CustomArrayList contains a specific element.
+     * 
+     * @param item The item to search for.
+     * @return true if the item exists in the list, otherwise false.
+     */
+    @Override
+    public boolean contains(T element) {
+        if (isEmpty()) return false;
+
+        for (int i = 0; i < size; i++) {
+            if (elements[i] != null && elements[i].equals(element)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     /**
      * Returns the number of elements in the list.
      */
@@ -46,7 +109,6 @@ public class CustomArrayList<T> implements CustomCollection<T> {
     public CustomIterator<T> iterator() {
         return new CustomArrayListIterator();
     }
-
 
     /**
      * Ensures that the internal array has enough capacity to add a new element.
