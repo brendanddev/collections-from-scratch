2701286524e7617890eb37c2de26b3021696888a
Brendan Dileo <dileob23@gmail.com>
2025-08-25 08:45:08 -0400

    Implement inner Node class

--- src/main/java/com/brendanddev/collections/list/CustomLinkedList.java
+++ src/main/java/com/brendanddev/collections/list/CustomLinkedList.java
@@ -1,7 +1,62 @@
 package com.brendanddev.collections.list;
 
+import com.brendanddev.collections.core.CustomCollection;
+import com.brendanddev.collections.core.CustomIterator;
+
+public class CustomLinkedList<T> implements CustomCollection<T> {
 
 
-public class CustomLinkedList {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /**
+     * A Node of a doubly linked list, holding a value a links to the previous and next nodes.
+     */
+    private static class Node<T> {
+        T value;
+        Node<T> prev;
+        Node<T> next;
+
+        /**
+         * Constructs a Node with a value, link to the previous node, and link to the next node.
+         * 
+         * @param value The value to store in this node.
+         * @param prev The link to the previous node.
+         * @param next The link to the next node.
+         */
+        Node(T value, Node<T> prev, Node<T> next) {
+            this.value = value;
+            this.prev = prev;
+            this.next = next;
+        }
+
+        /**
+         * Constructs a Node with only a value.
+         * 
+         * @param value The value to store in this node.
+         */
+        Node(T value) {
+            this(value, null, null);
+        }
+    }
     
 }
