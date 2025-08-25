7ed843cc5393c987f6865b34c225efa6d15c42fc
Brendan Dileo <dileob23@gmail.com>
2025-08-25 14:35:41 -0400

    Implement inner doubly node, and size and isEmpty methods for CustomDeque

--- src/main/java/com/brendanddev/collections/queue/CustomDeque.java
+++ src/main/java/com/brendanddev/collections/queue/CustomDeque.java
@@ -6,7 +6,55 @@ package com.brendanddev.collections.queue;
 
 
 
-
+/**
+ * A custom generic implementation of a Deque (double-ended queue).
+ */
 public class CustomDeque<T> {
+
+    private Node<T> head;
+    private Node<T> tail;
+    private int size = 0;
+
+    /** 
+     * Checks if the deque is empty.
+     */
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    /** 
+     * Returns the size of the deque.
+     */
+    public int size() {
+        return size;
+    }
+
+
+
+
+
+
+
+
+
+
+
+    /**
+     * A doubly linked Node used internally by the Deque.
+     */
+    private static class Node<T> {
+        T data;
+        Node<T> next;
+        Node<T> prev;
+
+        /**
+         * Constructs a new Node with the specified data.
+         * 
+         * @param data The data to store in this node.
+         */
+        Node(T data) {
+            this.data = data;
+        }
+    }
     
 }
