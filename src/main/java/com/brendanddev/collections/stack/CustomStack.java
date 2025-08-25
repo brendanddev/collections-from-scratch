fc9aa1c581133b612c83c4f4c88e8bf39db36605
Brendan Dileo <dileob23@gmail.com>
2025-08-25 11:50:00 -0400

    Implement push and pop methods

--- src/main/java/com/brendanddev/collections/stack/CustomStack.java
+++ src/main/java/com/brendanddev/collections/stack/CustomStack.java
@@ -1,5 +1,42 @@
 package com.brendanddev.collections.stack;
 
-public class CustomStack {
+import com.brendanddev.collections.list.CustomLinkedList;
+
+/**
+ * A custom generic stack implementation.
+ */
+public class CustomStack<T> {
+
+    private CustomLinkedList<T> linkedList;
+
+    /**
+     * Constructs an empty CustomStack.
+     */
+    public CustomStack() {
+        linkedList = new CustomLinkedList<>();
+    }
+
+    /**
+     * Pushes a new element onto the top of the stack.
+     * 
+     * @param value The value to push onto the stack.
+     */
+    public void push(T value) {
+        linkedList.add(value);
+    }
+
+    /**
+     * Removes and returns the top element of the stack.
+     * 
+     * @return The value at the top of the stack.
+     * @throws IllegalStateException if the stack is empty.
+     */
+    public T pop() {
+        if (linkedList.isEmpty()) {
+            throw new IllegalStateException("Stack is empty");
+        }
+        // Remove last element (top of stack)
+        return linkedList.removeAt(linkedList.size() - 1);
+    }
     
 }
