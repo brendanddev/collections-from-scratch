04d163ec518f7f70239cb562a318fd5b9c85b381
Brendan Dileo <dileob23@gmail.com>
2025-08-26 16:33:53 -0400

    Create CompareStack to compare performance between CustomStack and Java Util Stack

---
+++ src/main/java/com/brendanddev/collections/performance/CompareStack.java
@@ -0,0 +1,36 @@
+package com.brendanddev.collections.performance;
+
+
+
+
+
+
+public class CompareStack {
+
+
+
+
+
+
+         // Benchmark.measureWithResults("CustomStack push " + n + " elements", () -> {
+        //     for (int i = 0; i < n; i++) {
+        //         customStack.push(i);
+        //     }
+        //     return null;
+        // });
+
+        // Benchmark.measureWithResults("CustomStack peek " + n + " elements", () -> {
+        //     for (int i = 0; i < n; i++) {
+        //         customStack.peek();
+        //     }
+        //     return null;
+        // });
+
+        // Benchmark.measureWithResults("CustomStack pop " + n + " elements", () -> {
+        //     while (!customStack.isEmpty()) {
+        //         customStack.pop();
+        //     }
+        //     return null;
+        // });
+    
+}
