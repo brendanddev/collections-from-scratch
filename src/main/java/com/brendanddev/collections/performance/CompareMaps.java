c581a408450a7e7363988774b184ef262d9fcbc9
Brendan Dileo <dileob23@gmail.com>
2025-08-26 16:03:46 -0400

    Create CompareMaps

---
+++ src/main/java/com/brendanddev/collections/performance/CompareMaps.java
@@ -0,0 +1,15 @@
+package com.brendanddev.collections.performance;
+
+import java.util.HashMap;
+
+import com.brendanddev.collections.map.CustomHashMap;
+
+public class CompareMaps {
+
+    public static void benchmarkMaps(int n) {
+        CustomHashMap<Integer, String> customHashMap = new CustomHashMap<>();
+        HashMap<Integer, String> javaHashMap = new HashMap<>();
+    }
+
+
+}
