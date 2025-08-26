b950b8589ad948d02b98d4eeee73cad95e43a55d
Brendan Dileo <dileob23@gmail.com>
2025-08-25 20:33:59 -0400

    Implement contains, size, and isEmpty methods

--- src/main/java/com/brendanddev/collections/heap/CustomHeapList.java
+++ src/main/java/com/brendanddev/collections/heap/CustomHeapList.java
@@ -1,13 +1,69 @@
 package com.brendanddev.collections.heap;
 
 import com.brendanddev.collections.core.CustomCollection;
+import com.brendanddev.collections.core.CustomIterator;
 import com.brendanddev.collections.list.CustomArrayList;
 
 /**
- * A custom generic implementation of a Heap backed by a CustomArrayList.
+ * A custom generic implementation of a Min-Heap backed by a CustomArrayList.
+ * Uses the Comparable interface to maintain ordering of generic elements.
  */
 public class CustomHeapList<T extends Comparable<T>> implements CustomCollection<T> {
     
-    private CustomArrayList<T> list;
+    private final CustomArrayList<T> list;
+
+    /**
+     * Constructs an empty CustomHeapList.
+     */
+    public CustomHeapList() {
+        list = new CustomArrayList<>();
+    }
+
+
+    @Override
+    public boolean add(T element) {
+    }
+
+    @Override
+    public boolean remove(T element) {
+       
+    }
+
+    /**
+     * Checks if the heap contains the specified element.
+     * 
+     * @param element The element to check for.
+     * @return true if the element is found, otherwise false.
+     */
+    @Override
+    public boolean contains(T element) {
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).equals(element)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Returns the number of elements in the heap.
+     */
+    @Override
+    public int size() {
+        return list.size();
+    }
+
+    /**
+     * Checks if the heap is empty.
+     */
+    @Override
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+
+
+    @Override
+    public CustomIterator<T> iterator() {
+    }
     
 }
